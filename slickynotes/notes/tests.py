e6492ba830b174dc3b3f915443522926a613a05a
Abdulwahab Abdulmalik <abdulmalik.abdulwahab@andela.com>
2017-05-22 16:23:17 +0100

    Create Note model test

--- slickynotes/notes/tests.py
+++ slickynotes/notes/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
-
+from notes.models import Note
+from django.contrib.auth.models import User
 # Create your tests here.
+
+
+class TestModel(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='malikwahab',
+                                             password='test')
+
+    def test__str__(self):
+        note = Note.objects.create(title="A simple Note",
+                                   content="A Jot during assessment",
+                                   owner=self.user)
+        self.assertEqual(note.__str__(), "A simple Note")
